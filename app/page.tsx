c7f5a0c01c491bd417ea5e1eae0577fb45b34c98
naman005 <namanchaturvedi05@gmail.com>
2026-01-21 16:40:26 +0530

    updated portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,14 @@ import image1 from "../public/expense-tracker.png";
 import image2 from "../public/portfolio.png";
 import image3 from "../public/simon.png";
 import image4 from "../public/moneycare.png";
+import image5 from "../public/food-app.png"
 
 
 
 export default function Home() {
   return (
         <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
-        <div className="mx-auto max-w-xl px-4 py-20">
+        <div className="mx-auto max-w-xl px-4 py-8">
           <motion.header
           initial={{opacity: 0, x: -100}}
           animate={{opacity: 1, x: 0}}
@@ -27,13 +28,13 @@ export default function Home() {
 
           className="flex items-center justify-between mb-12">
           <div className="w-20 h-20 rounded-full overflow-hidden">
-            <Image src={profile} alt="profile picture" className="cursor-pointer transition-all duration-300 hover:scale-110"></Image>
+            <Image src={profile} alt="profile picture" className="transition-all duration-300 hover:scale-110"></Image>
           </div>
           <ThemeToggle />
           </motion.header>
 
           <main className="space-y-10">
-          <section className="space-y-10">
+          <section className="space-y-4">
           <motion.div 
           initial={{opacity: 0, x: 100}}
           animate={{opacity: 1, x: 0}}
@@ -43,8 +44,7 @@ export default function Home() {
           <h1 className="text-4xl font-bold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
             Naman Chaturvedi
           </h1>
-          <p className="text-gray-600 dark:text-gray-400">Web Developer & Aspiring SDE</p>
-          <p className="text-gray-600 dark:text-gray-400">India</p>
+          <p className="text-gray-600 dark:text-gray-400">Aspiring Software Engineer</p>
           </motion.div>
 
           <motion.div
@@ -53,9 +53,9 @@ export default function Home() {
           transition={{duration: 0.8}}
           className="space-y-3"
           >
-            <h2 className="text-lg font-semibold">Building Scalable Web Solutions for Tomorrow</h2>
+            <h2 className="text-lg font-semibold">Learning to Build Scalable and Robust Systems</h2>
             <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2xl">
-              I &apos; m a <span className="text-black dark:text-white">MERN </span>stack web developer focused on building clean, responsive, and scalable web applications.
+              I&apos;m a <span className="text-black dark:text-white">MERN </span>stack web developer focused on building clean, responsive, and scalable web applications.
               I work with <span className="text-black dark:text-white"> MongoDB, Express, React, and Node.js, </span> and I also use  <span className="text-black dark:text-white">PostgreSQL </span> for robust relational data handling.
 From designing <span className="text-black dark:text-white">RESTful APIs </span>to deploying full-stack apps, I create solutions that are fast, user-friendly, and built to grow.
             </p>
@@ -86,18 +86,20 @@ From designing <span className="text-black dark:text-white">RESTful APIs </span>
           initial={{opacity: 0, y: 100}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 0.8}}
-          className="space-y-8"
+          className="space-y-4"
           >
           <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">Education</h2>
-          <div className="space-y-2">
-            <div className="flex items-center justify-between">
+          <div className="space-y-4">
               <div className="space-y-1">
-                <p className="text-sm text-gray-600 dark:text-gray-400">July, 2022 - 2025</p>
+                <p className="font-medium">Master of Computer Applications</p>
+                <p className="text-sm text-gray-600 dark:text-gray-400">Aug, 2025 - Expected 2027</p>
+                <p className="text-blue-600 dark:text-blue-400">GLA University, Mathura</p>
+              </div>
+              <div className="space-y-1">
                 <p className="font-medium">Bachelor of Computer Applications</p>
+                <p className="text-sm text-gray-600 dark:text-gray-400">July, 2022 - 2025 | CGPA - 7.87</p>
                 <p className="text-blue-600 dark:text-blue-400">Sanskriti University, Mathura</p>
               </div>
-
-            </div>
 
           </div>
           </motion.section>
@@ -112,6 +114,34 @@ From designing <span className="text-black dark:text-white">RESTful APIs </span>
   </h2>
 
   <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <Card className="bg-gray-50 dark:bg-zinc-900 border border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105"
+      >
+        <CardContent>
+          <Image src={image5} alt="food-app" className="rounded-lg mb-4 w-full h-auto" />
+          <div className="flex items-center justify-between">
+            <div>
+              <h3 className="font-medium">QR-based Self Ordering System</h3>
+              <p className="text-sm text-gray-600 dark:text-gray-400">React, Firebase, shadcn/ui</p>
+            </div>
+            <div>
+             <Link href="https://github.com/naman005/food-app" className="cursor-pointer text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors">
+
+            <Button variant="ghost">
+          <FaGithub className="w-6 h-6" />
+            </Button>
+            </Link>
+
+            <Link href="https://food-app-five-ruby.vercel.app/" className="cursor-pointer text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors">
+
+            <Button variant="ghost">
+          <FiExternalLink className="w-6 h-6" />
+            </Button>
+            </Link>
+
+            </div>
+          </div>
+        </CardContent>
+      </Card>
       <Card className="bg-gray-50 dark:bg-zinc-900 border border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105"
       >
         <CardContent>
@@ -237,7 +267,7 @@ From designing <span className="text-black dark:text-white">RESTful APIs </span>
   </div>
 
 </motion.section>
-  <p className="text-gray-500">© 2025 Naman Chaturvedi</p>
+  <p className="text-gray-500">© 2026 Naman Chaturvedi</p>
 </main>
         </div>
         </div>
